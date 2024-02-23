3fd7cc3dac361e25769ede3c5e85350c665fa92a
Isac Santos <isacjobz@gmail.com>
2024-02-23 18:32:31 -0300

    configurando o modal
    foi realizado o design e os inputs

---
+++ js/modal.js
@@ -0,0 +1,20 @@
+const exampleModal = document.getElementById('exampleModal');
+const titulModal = document.getElementsByClassName("modal-title");
+
+
+if (exampleModal) {
+  exampleModal.addEventListener('show.bs.modal', event => {
+    // Button that triggered the modal
+    const button = event.relatedTarget
+    // Extract info from data-bs-* attributes
+    const recipient = button.getAttribute('data-bs-whatever')
+    // If necessary, you could initiate an Ajax request here
+    // and then do the updating in a callback.
+    // Update the modal's content.
+    const modalTitle = exampleModal.querySelector('.modal-title')
+    const modalBodyInput = exampleModal.querySelector('.modal-body input')
+
+    modalTitle.textContent = `Envie um Email para ${recipient} :)`
+    modalBodyInput.value = 
+  })
+}
