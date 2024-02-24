01df94b3be5a7b42d915e62b1d59164f50094177
Isac Santos <isacjobz@gmail.com>
2024-02-24 19:15:41 -0300

    Finalizado o model e o alert
    foi feito os ajustes finais

--- js/alert.js
+++ js/alert.js
@@ -1,4 +1,8 @@
-const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder');
+const submit = document.getElementById("liveAlertBtn");
+const email = document.getElementById("recipient-name");
+const mensagem = document.getElementById("message-text");
+submit.disabled = true;
 const appendAlert = (message, type) => {
   const wrapper = document.createElement('div')
   wrapper.innerHTML = [
@@ -13,8 +17,34 @@ const appendAlert = (message, type) => {
 
 const alertTrigger = document.getElementById('liveAlertBtn')
 if (alertTrigger) {
-  alertTrigger.addEventListener('submit', () => {
+  alertTrigger.addEventListener('click', () => {
     appendAlert("Email enviado com Sucesso !", 'success')
     
   })
 }
+
+email.addEventListener('input', validarInput);
+mensagem.addEventListener('input', validarInput);
+
+submit.addEventListener("mouseover", function(){
+  submit.style.backgroundColor = '#222831'
+} )
+
+submit.addEventListener("mouseout", function(){
+  submit.style.backgroundColor = '#00ADB5'
+} )
+
+function validarInput(){
+  if(email.value.trim() === "" || mensagem.value.trim() === "" ){
+    submit.disabled = true;
+    console.log(email.value.trim())
+    submit.style.backgroundColor = 'gray';
+  }else if(email.value.trim() != "" || mensagem.value.trim() != "" ){
+    submit.disabled = false;
+    console.log(email.value.trim())
+    submit.style.backgroundColor = '#00ADB5';
+    
+    }
+    
+  }
+    
